2224c66122f7d093578def8ed9926b26c948273e
Tinotenda Muringami <55862003+mrhoodz@users.noreply.github.com>
2026-01-17 23:06:04 +0000

    added env

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify("AIzaSyDsOSvEBdGXfFKsHGUOEPJPF-n17W7aiek"),
+        'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyDsOSvEBdGXfFKsHGUOEPJPF-n17W7aiek")
       },
       resolve: {
         alias: {
